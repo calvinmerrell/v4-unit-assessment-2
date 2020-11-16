69020f4892083c68833758e2124efb0920ebc518
Calvin Merrell <calvinmerrell@gmail.com>
2020-11-15 21:31:51 -0700

    html css finished

--- js-in-the-dom/index.js
+++ js-in-the-dom/index.js
@@ -1,1 +1,21 @@
+const formBtn = document.getElementById("close-form");
+const form = document.getElementById("form");
 
+if (formBtn.innerText === 'X') {
+    formBtn.innerText = '+';
+    clickedElement.innerText = "X";
+} else {
+    formBtn.innerText = 'X';
+    clickedElement.innerText = "+";
+}
+
+
+    // if (playerSpan.innerText === 'X') {
+    //     playerSpan.innerText = 'O';
+       
+    //     board[clickedId] = "X";
+    // } else {
+    //     playerSpan.innerText = 'X';
+    //     clickedElement.innerText = "O";
+    //     board[clickedId] = "O";
+    // }
